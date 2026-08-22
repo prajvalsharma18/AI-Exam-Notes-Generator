f52288ddfb6d8cfeb1ac8675df9aa0a5d8b4022a
prajvalsharma <prajval_2024bcse018@nitsri.ac.in>
2026-08-23 02:05:18 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ import Notes from './pages/Notes'
 import Pricing from './pages/Pricing'
 import PaymentSuccess from './pages/PaymentSuccess'
 import PaymentFailed from './pages/PaymentFailed'
-export const serverUrl = "http://localhost:8000"
+export const serverUrl = "https://ai-exam-notes-generator-backend-v9rn.onrender.com"
 
 function App() {
   const dispatch = useDispatch()
