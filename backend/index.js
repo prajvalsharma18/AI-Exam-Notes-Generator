fc99d0ff2fde5e1c92497c5b3282a1f0c06e0333
prajvalsharma <prajval_2024bcse018@nitsri.ac.in>
2026-08-23 02:21:28 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.post(
 );
 
 app.use(cors(
-    {origin:"http://localhost:5173",
+    {origin:"https://ai-exam-notes-generator-7iyh.onrender.com",
         credentials:true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
     }
